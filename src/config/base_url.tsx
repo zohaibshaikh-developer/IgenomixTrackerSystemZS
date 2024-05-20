d09a93ef9a66f1dc5893748726857f50349bbf8a
zohaibshaikh-developer <zohaibshaikhdeveloper@gmail.com>
2024-05-21 03:09:55 +0530

    Vaibhav Bhai Changes Add Dropdown OF Clinic  In Overview

--- src/config/base_url.tsx
+++ src/config/base_url.tsx
@@ -1,6 +1,6 @@
 // base_url.js
-// const BASE_URL = 'https://brave-octopus.cyclic.app';
+const BASE_URL = 'https://brave-octopus.cyclic.app';
 
-const BASE_URL = 'http://localhost:3000';
+// const BASE_URL = 'http://localhost:3000';
 
 export default BASE_URL;
